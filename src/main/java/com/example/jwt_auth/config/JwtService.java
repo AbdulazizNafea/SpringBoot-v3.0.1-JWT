2626bf24b67e37dd408e6f6f129b143c657d9de2
Aziz <AbdulazizAlfayzi@outlook.com>
2023-06-15 16:49:26 +0300

    update JwtService

--- src/main/java/com/example/jwt_auth/config/JwtService.java
+++ src/main/java/com/example/jwt_auth/config/JwtService.java
@@ -2,15 +2,20 @@ package com.example.jwt_auth.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 public class JwtService {
 
-    private static final String SECRET_KEY = "zbRyDfRMVKtLYbGUzhvXaAUN8pso83oHdVvJ3LAH1d1A8xN9+73oOzfWV6EsrU00HO5/PeSaYtvlHBJDgKZoszZ5l1U0nITKtNvoh6vkZ0Sl6KKweqjLM+ZtlvjfyYUlT6zy53mKmQLg/ajZQKbtszb5E1r2Xn73SeCVmY+w4tsT2k0E9nUuO61lOZDOfL8mfIFDHgi7yjLhWHhUZgPXjdezmD6FJHOwTLBlinIumSFZaBjx8LkYL0+RBuH4mPEl0DscrnEJvs2TILESBKSaPa31T1ex5xgNq0fkrzUhD2cs1oyj+VSxCX9K1N7o/Tkue4cKf0JyIy04E+iyyTkTw95mmbPEioshU/FgFrTqNJk=\n";
+    private static final String SECRET_KEY = "ZqP91vd36ivfA9BwJr+kQuioNGEm3es+eF0kA/t6I+vBwPSYcITnphYHNjow39RwHNSATj2iaTp4C4oiLf9v8g==";
     public String extractUsername(String token) {
         return extractClaim(token,Claims::getSubject);
     }
@@ -18,6 +23,34 @@ public class JwtService {
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String,Object> extraClaims, UserDetails userDetails){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSigningKey(), SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
     }
 
     private Claims extractAllClaims(String token) {
